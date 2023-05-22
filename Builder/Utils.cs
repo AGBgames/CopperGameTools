148415f39b66c515c9edc24a1cc2e58ba273ad9e
= <nils19052006@gmail.com>
2023-05-22 20:29:03 +0200

    added util class

---
+++ Builder/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopperGameTools.Builder
+{
+    public class Utils
+    {
+        public static string GetVersion() { return "0.4.1"; }
+        public static string GetCopyright() { return "AGBgames 2023"; }
+    }
+}
