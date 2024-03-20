2ba878c5df2097c9fc01e21ca9a6c5058dc85e07
AGBDev <nils19052006@gmail.com>
2024-03-20 13:30:47 +0100

    g

--- Shared/Constants.cs
+++ Shared/Constants.cs
@@ -1,10 +1,9 @@
-﻿namespace CopperGameTools.Shared
+﻿namespace CopperGameTools.Shared;
+
+public static class Constants
 {
-    public static class Constants
-    {
-        public static readonly string Version = "0.4.6";
-        public static readonly string SupportedCopperCubeVersion = "6.6";
-        public static readonly string BuildDate = "20.03.24 [DD.MM.YY]";
-        public static readonly string Copyright = "AGBgames 2024";
-    }
+    public static readonly string Version = "0.4.6";
+    public static readonly string SupportedCopperCubeVersion = "6.6";
+    public static readonly string BuildDate = "20.03.24 [DD.MM.YY]";
+    public static readonly string Copyright = "AGBgames 2024";
 }
