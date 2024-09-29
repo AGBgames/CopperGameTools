733f60f31ef48a6f2da2cb3889e3e0001436b5e8
AGBDev <nils19052006@gmail.com>
2024-09-29 15:59:25 +0200

    ..-

--- Shared/CopperGameToolsInfo.cs
+++ Shared/CopperGameToolsInfo.cs
@@ -3,7 +3,7 @@
 public static class CopperGameToolsInfo
 {
     public const string MajorVersion = "1.2.*";
-    public const string Version = "1.2.5";
+    public const string Version = "1.2.3";
     public const string SupportedCopperCubeVersion = "6.7";
-    public const string BuildDate = "28.09.2024 [DD.MM.YY]";
+    public const string BuildDate = "29.09.2024 [DD.MM.YY]";
 }
