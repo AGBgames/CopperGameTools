03b45bd59a3a34eb5ca63a5ae0c03eb454196c64
AGBDev <nils19052006@gmail.com>
2024-10-06 15:21:19 +0200

    fgfg

--- Shared/Utils.cs
+++ Shared/Utils.cs
@@ -2,6 +2,11 @@
 
 public abstract class Utils
 {
+    /// <summary>
+    /// A Valid string is neither null, empty nor just a whitespace.
+    /// </summary>
+    /// <param name="str">String to check</param>
+    /// <returns>Boolean indicating if the string is valid (defined by the rules)</returns>
     public static bool IsValidString(string str)
     {
         return !string.IsNullOrEmpty(str) && !string.IsNullOrWhiteSpace(str);
