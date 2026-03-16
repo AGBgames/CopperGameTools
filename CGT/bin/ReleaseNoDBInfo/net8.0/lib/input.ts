753030e84b5db3e40af29867720d8a2f6ae72424
AGBDev <nils19052006@gmail.com>
2026-03-16 22:05:03 +0100

    added content packer

---
+++ CGT/bin/ReleaseNoDBInfo/net8.0/lib/input.ts
@@ -0,0 +1,87 @@
+type KeyCodes = {
+    escape: 27;
+    space: 32;
+
+    digit0: 48;
+    digit1: 49;
+    digit2: 50;
+    digit3: 51;
+    digit4: 52;
+    digit5: 53;
+    digit6: 54;
+    digit7: 55;
+    digit8: 56;
+    digit9: 57;
+
+    a: 65;
+    b: 66;
+    c: 67;
+    d: 68;
+    e: 69;
+    f: 70;
+    g: 71;
+    h: 72;
+    i: 73;
+    j: 74;
+    k: 75;
+    l: 76;
+    m: 77;
+    n: 78;
+    o: 79;
+    p: 80;
+    q: 81;
+    r: 82;
+    s: 83;
+    t: 84;
+    u: 85;
+    v: 86;
+    w: 87;
+    x: 88;
+    y: 89;
+    z: 90;
+};
+
+const keyCodes: KeyCodes = {
+    escape: 27,
+    space: 32,
+
+    digit0: 48,
+    digit1: 49,
+    digit2: 50,
+    digit3: 51,
+    digit4: 52,
+    digit5: 53,
+    digit6: 54,
+    digit7: 55,
+    digit8: 56,
+    digit9: 57,
+
+    a: 65,
+    b: 66,
+    c: 67,
+    d: 68,
+    e: 69,
+    f: 70,
+    g: 71,
+    h: 72,
+    i: 73,
+    j: 74,
+    k: 75,
+    l: 76,
+    m: 77,
+    n: 78,
+    o: 79,
+    p: 80,
+    q: 81,
+    r: 82,
+    s: 83,
+    t: 84,
+    u: 85,
+    v: 86,
+    w: 87,
+    x: 88,
+    y: 89,
+    z: 90
+};
+
+const Input: {keyCodes: KeyCodes} = {keyCodes: keyCodes}
---
+++ ContentPacker/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ContentPacker;
+
+using System.IO;
+using System.Text;
+
+public class ContentPacker
+{
+    public static void CreatePackage(string outputPath, string[] filesToPack)
+    {
+        using (var fs = new FileStream(outputPath, FileMode.Create))
+        using (var writer = new BinaryWriter(fs, Encoding.UTF8))
+        {
+            // 1. Header schreiben
+            writer.Write("GPACK"u8.ToArray()); // Magic Number
+            writer.Write((short)1); // Dateiformat Version
+            writer.Write((short)100); // Engine Version (1.0.0)
+
+            // 2. Platzhalter für TOC (Table of Contents).
+            // Wir müssen später wissen, wie viele Dateien drin sind
+            writer.Write(filesToPack.Length);
+
+            long tocPosition = fs.Position;
+            // Wir reservieren Platz für Offsets (später wichtig zum Springen)
+            // Pro Datei: Name (String), Offset (Long), Size (Int).
+            // Einfachheitshalber schreiben wir die Daten erst und merken uns die Positionen.
+
+            // 3. Daten schreiben & Metadaten sammeln
+            var fileEntries = new List<(string name, long offset, int size)>();
+
+            // Wir springen über den TOC-Bereich hinaus und schreiben erst die Daten
+            fs.Seek(tocPosition + (filesToPack.Length * 256), SeekOrigin.Begin);
+
+            foreach (var file in filesToPack)
+            {
+                byte[] data = File.ReadAllBytes(file);
+                long offset = fs.Position;
+                writer.Write(data);
+                fileEntries.Add((Path.GetFileName(file), offset, data.Length));
+            }
+
+            // 4. TOC zurückschreiben
+            fs.Seek(tocPosition, SeekOrigin.Begin);
+            foreach (var entry in fileEntries)
+            {
+                writer.Write(entry.name.PadRight(100)); // Fixe Länge für einfachen Zugriff
+                writer.Write(entry.offset);
+                writer.Write(entry.size);
+            }
+        }
+    }
+}
