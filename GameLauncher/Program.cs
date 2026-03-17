be1d2b72cea04e8b1d872ffdd2f68e19cd55372a
AGBDev <nils19052006@gmail.com>
2026-03-17 15:02:43 +0100

    h

--- GameLauncher/Program.cs
+++ GameLauncher/Program.cs
@@ -1,14 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
+using System.Text;
+
 public class Program
 {
     private static readonly string GARCHIVE_LIST = "gpacklist.txt";
     private static readonly string GARCHIVE_INDEX_LIST = "gpackindex.txt";
 
+    private static readonly string MODS_PATH = "TDR3/Mods/";
+
+    private static List<string> gameArguments = [];
+
     public static void Main(string[] args)
     {
         List<string> loaded = LoadGameArchives();
         Console.WriteLine("Loaded {0} archives!", loaded.Count);
+
+        if (args.Length == 0)
+            return;
+
+        string what = args[0];
+        if (what == "legacy")
+        {
+            RunGame();
+        }
+        else if (what == "modded")
+        {
+            RunGameModded();
+        }
     }
 
     private static List<string> LoadGameArchives()
@@ -48,5 +68,54 @@ public class Program
         File.WriteAllLines(GARCHIVE_INDEX_LIST, loadedArchives);
         
         return loadedArchives;
+    }
+
+    private static void RunGame()
+    {
+        Process gameProcess = new Process();
+        gameProcess.StartInfo.FileName = "TheDarkRooms3";
+        foreach (string gameArgument in gameArguments)
+        {
+            gameProcess.StartInfo.ArgumentList.Add(gameArgument);
+        }
+        gameProcess.Start();
+        gameProcess.WaitForExit();
+    }
+
+    private static void RunGameModded()
+    {
+        string modLog = "";
+        string[] modFolders = Directory.GetDirectories(MODS_PATH, "*", SearchOption.TopDirectoryOnly);
+        
+        StringBuilder moddedScript = new StringBuilder(File.ReadAllText(Path.Combine(MODS_PATH, "TheDarkRooms3.js")));
+        moddedScript.AppendLine("Mods.runningModLoader = true;");
+        
+        foreach (string modFolder in modFolders)
+        {
+            DirectoryInfo modDirectory = new DirectoryInfo(modFolder);
+            string modName = modDirectory.Name;
+            
+            Console.WriteLine("Found mod {0}", modName);
+            
+            moddedScript.AppendLine($"// Mod Start {modName} //");
+
+            moddedScript.AppendLine($"var {modName} = {{SettingsPath: '{modFolder}/Settings'}};");
+            
+            string modMain = $"{modFolder}/Source/Main.js";
+            moddedScript.AppendLine(File.ReadAllText(modMain));
+            moddedScript.AppendLine($"{modName}Main();");
+            
+            moddedScript.AppendLine($"Mods.loadedMods.push('{modName}');");
+            
+            moddedScript.AppendLine($"// Mod End {modName} //");
+        }
+        
+        File.WriteAllText(Path.Combine(MODS_PATH, "Modded.js"), moddedScript.ToString());
+        
+        gameArguments.Add("-script:");
+        gameArguments.Add(Path.Combine(MODS_PATH, "Modded.js"));
+        gameArguments.Add("-debug");
+        
+        RunGame();
     }
 }
